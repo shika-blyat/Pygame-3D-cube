0f1364c6b6eb6429c548a052ecdb780dfe4b0458
shika-blyat <54486529+shika-blyat@users.noreply.github.com>
2019-09-02 05:14:10 +0200

    Add files via upload

---
+++ TestCubeWorking copy.py
@@ -0,0 +1,133 @@
+import sys, math, pygame
+from operator import itemgetter
+angle = 0
+class Point3D:
+    def __init__(self, x = 0, y = 0, z = 0):
+        self.x, self.y, self.z = float(x), float(y), float(z)
+ 
+    def rotateX(self, angle):
+        """ Rotates the point around the X axis by the given angle in degrees. """
+        rad = angle * math.pi / 180
+        cosa = math.cos(rad)
+        sina = math.sin(rad)
+        y = self.y * cosa - self.z * sina
+        z = self.y * sina + self.z * cosa
+        return Point3D(self.x, y, z)
+ 
+    def rotateY(self, angle):
+        """ Rotates the point around the Y axis by the given angle in degrees. """
+        rad = angle * math.pi / 180
+        cosa = math.cos(rad)
+        sina = math.sin(rad)
+        z = self.z * cosa - self.x * sina
+        x = self.z * sina + self.x * cosa
+        return Point3D(x, self.y, z)
+ 
+    def rotateZ(self, angle):
+        """ Rotates the point around the Z axis by the given angle in degrees. """
+        rad = angle * math.pi / 180
+        cosa = math.cos(rad)
+        sina = math.sin(rad)
+        x = self.x * cosa - self.y * sina
+        y = self.x * sina + self.y * cosa
+        return Point3D(x, y, self.z)
+ 
+    def project(self, win_width, win_height, fov, viewer_distance):
+        """ Transforms this 3D point to 2D using a perspective projection. """
+        factor = fov / (viewer_distance + self.z)
+        x = self.x * factor + win_width / 2
+        y = -self.y * factor + win_height / 2
+        return Point3D(x, y, self.z)
+
+class Simulation:
+    def __init__(self, win_width = 640, win_height = 480):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((win_width, win_height))
+        pygame.display.set_caption("3D Cube")
+        
+        self.clock = pygame.time.Clock()
+
+        self.vertices = [
+            Point3D(-1,1,-1),
+            Point3D(1,1,-1),
+            Point3D(1,-1,-1),
+            Point3D(-1,-1,-1),
+            Point3D(-1,1,1),
+            Point3D(1,1,1),
+            Point3D(1,-1,1),
+            Point3D(-1,-1,1)
+        ]
+
+        # Define the vertices that compose each of the 6 faces. These numbers are
+        # indices to the vertices list defined above.
+        self.faces  = [(0,1,2,3),(1,5,6,2),(5,4,7,6),(4,0,3,7),(0,4,5,1),(3,2,6,7)]
+
+        # Define colors for each face
+        self.colors = [(255,0,255),(255,0,0),(0,255,0),(0,0,255),(0,255,255),(255,255,0)]
+
+        
+    def run(self,side):
+        global angle
+        for Coucou_a_toi_qui_lis_mon_code in range(30):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                        
+            self.clock.tick(50)
+            self.screen.fill((0,32,0))
+            t = []
+            
+            if side == 'right' or side =='left':
+                for v in self.vertices:
+                    r = v.rotateY(angle)
+                    p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
+                    t.append(p)
+            elif side == 'top' or side == 'bot':
+                for v in self.vertices:
+                    r = v.rotateX(angle)
+                    #rotateZ(angle)
+                    p = r.project(self.screen.get_width(), self.screen.get_height(), 256, 4)
+                    t.append(p)
+
+            # calculate the z of each face.
+            avg_z = []
+            i = 0
+            for f in self.faces:
+                z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
+                avg_z.append([i,z])
+                i +=1
+
+            for tmp in sorted(avg_z,key=itemgetter(1),reverse=True):
+                face_index = tmp[0]
+                f = self.faces[face_index]
+                pointlist = [(t[f[0]].x, t[f[0]].y), (t[f[1]].x, t[f[1]].y),
+                             (t[f[1]].x, t[f[1]].y), (t[f[2]].x, t[f[2]].y),
+                             (t[f[2]].x, t[f[2]].y), (t[f[3]].x, t[f[3]].y),
+                             (t[f[3]].x, t[f[3]].y), (t[f[0]].x, t[f[0]].y)
+                             ]
+                pygame.draw.polygon(self.screen,self.colors[face_index],pointlist)
+
+            if side == 'right' or side == 'top':
+                print(side)
+                angle += 1
+            elif side == 'left' or side == 'bot':
+                print('-')
+                angle-=1       
+            print(angle)     
+            pygame.display.flip()
+    def start(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        Simulation().run('right')
+                    if event.key == pygame.K_LEFT:
+                        Simulation().run('left')
+                    if event.key == pygame.K_UP:
+                        Simulation().run('top')
+                    if event.key == pygame.K_DOWN:
+                        Simulation().run('bot')
+Simulation().start()
